84df9667b84bf8ffc1e6d9b87717e6b9295de1fd
Chris Martin <ch.martin@gmail.com>
2023-03-08 14:24:12 -0700

    support gambler-0.4

--- default.nix
+++ default.nix
@@ -26,7 +26,7 @@ ghc."9.4" = nixos-22-11.haskell.packages.ghc94.override (old: {
     overrides = combineOverrides old [
         sourceOverrides
         (new: old: {
-            gambler = new.callPackage ./nix/gambler-0.3.0.0.nix {};
+            gambler = new.callPackage ./nix/gambler-0.4.0.0.nix {};
             quaalude = new.callPackage ./nix/quaalude.nix {};
         })
     ];
